3152a7868caea8fc25f86d8b7cf05a6f6203b0f4
Charliearlie <cw5790@gmail.com>
2017-02-06 20:24:34 +0000

    Fixed Heroku redirect

--- public/services/authenticate.js
+++ public/services/authenticate.js
@@ -2,8 +2,8 @@ angular.module('Scrbbl')
   .factory('Authenticate', ['$http', '$window', function($http, $window) {
       return {
         login: function() {
-            return $window.location.href = 'http://www.last.fm/api/auth/?api_key=f4f831e3766b42660d26c9b29b25599e&cb=http://localhost:3000/';
-            //return $window.location.href = 'http://www.last.fm/api/auth/?api_key=f4f831e3766b42660d26c9b29b25599e&cb=http://scrbbl.herokuapp.com/';
+            //return $window.location.href = 'http://www.last.fm/api/auth/?api_key=f4f831e3766b42660d26c9b29b25599e&cb=http://localhost:3000/';
+            return $window.location.href = 'http://www.last.fm/api/auth/?api_key=f4f831e3766b42660d26c9b29b25599e&cb=http://scrbbl.herokuapp.com/';
         },
         createSession: function(token) {
           return $http.get('/api/auth/' + token);
