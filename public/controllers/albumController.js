e3cfb35159795ec16a9e2ae7f78125fa8518ab91
Charliearlie <cw5790@gmail.com>
2017-02-08 22:23:38 +0000

    Added album controller

---
+++ public/controllers/albumController.js
@@ -0,0 +1,25 @@
+angular.module('Scrbbl')
+    .controller('ScrobbleAlbumCtrl', ['$scope', '$alert', 'Authenticate', '$location', 'Scrobble',
+    function($scope, $alert, Authenticate, $location, Scrobble) {
+        $scope.scrobble = function() {
+            Scrobble.searchAlbum($scope.album).then(function(result) {
+                //$scope.results = result.data;
+                $scope.results = _.map(result.data.results.albummatches.album, function(result) {
+                    return {
+                        title: result.name,
+                        artist: result.artist
+                    };
+                });
+                console.log($scope.results);
+            });
+        };
+
+        $scope.selectResult = function(title, artist) {
+            $scope.album.title = title;
+            $scope.album.artist = artist;
+
+            Scrobble.scrobbleAlbum($scope.album).then(function(result) {
+                console.log(result);
+            });
+        }
+    }]);
