8b07a70a3ed059f12ec4589dcba49f288c108a95
unknown <charlesw@fossa.mentalbox.local>
2017-02-10 15:57:24 +0000

    After login the user will now be redirected back to the page they were on

--- public/services/authenticate.js
+++ public/services/authenticate.js
@@ -1,9 +1,10 @@
 angular.module('Scrbbl')
-  .factory('Authenticate', ['$http', '$window', function($http, $window) {
+  .factory('Authenticate', ['$http', '$window', '$location', function($http, $window, $location) {
       return {
         login: function() {
+            var callbackUrl = $location.absUrl().split('?')[0];
             //return $window.location.href = 'http://www.last.fm/api/auth/?api_key=f4f831e3766b42660d26c9b29b25599e&cb=http://localhost:3000/';
-            return $window.location.href = 'http://www.last.fm/api/auth/?api_key=f4f831e3766b42660d26c9b29b25599e&cb=http://scrbbl.herokuapp.com/';
+            return $window.location.href = 'http://www.last.fm/api/auth/?api_key=f4f831e3766b42660d26c9b29b25599e&cb=' + callbackUrl;
         },
         createSession: function(token) {
             return $http.get('/api/auth/' + token);
