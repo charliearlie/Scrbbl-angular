97f11f1e95f166e3539a9cc0be2f58aa5937cb85
Charliearlie <cw5790@gmail.com>
2017-03-04 10:20:47 +0000

    Scrobble from user now displays users last 50 played tracks

--- public/js/controllers/scrobbleUserController.js
+++ public/js/controllers/scrobbleUserController.js
@@ -1,12 +1,10 @@
 (function () {
-    app.controller('ScrobbleUserCtrl', ['$scope', 'LastfmUser', scrobbleUserController]);
-
-    function scrobbleUserController($scope, LastfmUser) {
-        $scope.lastfmUser = '';
-
+    app.controller('ScrobbleUserCtrl', ['$scope', 'LastfmUser', 
+    function($scope, LastfmUser) {
+        $scope.lastfmuser = {};
         $scope.searchUser = function() {
-            LastfmUser.getUserData($scope.lastfmUser)
-                .then(function(result) {
+            LastfmUser.getUserData($scope.lastfmuser.name)
+                .then(function(tracks) {
                     $scope.results = _.map(tracks, function (track) {
                             return {
                                 title: track.name,
@@ -20,6 +18,13 @@
 
                 });
         }
-    }
+
+        $scope.$watch('userToSearch', _.debounce(function (user, newVal) {
+                $scope.$apply(function () {
+                    console.log($scope.userToSearch);
+
+                });
+            }, 1000), true);
+    }]);
 
 } ());
